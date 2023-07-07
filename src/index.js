fe4892c8793626de680d89e66e4a26bf440990ea
Peter-Kibet <peter.kibet@hotmail.com>
2023-07-07 15:25:55 +0300

    Add arrow function

--- src/index.js
+++ src/index.js
@@ -1,35 +1,36 @@
-import './style.css';
-import axios from 'axios';
+import "./style.css";
+import axios from "axios";
 
-const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
-const gameID = 'YOUR_GAME_ID'; // Replace with the game ID obtained from the API
+const baseURL =
+  "https://us-central1-js-capstone-backend.cloudfunctions.net/api/";
+const gameID = "YOUR_GAME_ID"; // Replace with the game ID obtained from the API
 
-const scoreTable = document.querySelector('.score-table');
-const refreshButton = document.querySelector('.refresh');
-const scoreForm = document.querySelector('#score-form');
-const nameInput = document.querySelector('#name');
-const scoreInput = document.querySelector('#score');
+const scoreTable = document.querySelector(".score-table");
+const refreshButton = document.querySelector(".refresh");
+const scoreForm = document.querySelector("#score-form");
+const nameInput = document.querySelector("#name");
+const scoreInput = document.querySelector("#score");
 
 // Function to fetch and display scores
-async function refreshScores() {
+const refreshScores = async () => {
   try {
     const response = await axios.get(`${baseURL}games/${gameID}/scores/`);
     const scores = response.data.result;
 
-    scoreTable.innerHTML = '';
+    scoreTable.innerHTML = "";
 
     scores.forEach((score) => {
-      const scoreElement = document.createElement('div');
+      const scoreElement = document.createElement("div");
       scoreElement.textContent = `User: ${score.user} - Score: ${score.score}`;
       scoreTable.appendChild(scoreElement);
     });
   } catch (error) {
     // Handle the error without using any console statements
   }
-}
+};
 
 // Event listener for Refresh button
-refreshButton.addEventListener('click', refreshScores);
+refreshButton.addEventListener("click", refreshScores);
 
 // Function to add a new score
 async function addScore(event) {
@@ -48,8 +49,8 @@ async function addScore(event) {
       score,
     });
 
-    nameInput.value = '';
-    scoreInput.value = '';
+    nameInput.value = "";
+    scoreInput.value = "";
     refreshScores();
   } catch (error) {
     // Handle the error without using any console statements
@@ -57,7 +58,7 @@ async function addScore(event) {
 }
 
 // Event listener for Submit button
-scoreForm.addEventListener('submit', addScore);
+scoreForm.addEventListener("submit", addScore);
 
 // Initial fetch of scores when the page loads
 refreshScores();
